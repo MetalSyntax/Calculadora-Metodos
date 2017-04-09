eadb49e5aefdc9f2dc552013489eaf64bc48b54b
MetalSyntax <wonderdxk@gmail.com>
2017-04-09 18:04:37 -0400

    1.0.5

--- js/SideNav.js
+++ js/SideNav.js
@@ -4,7 +4,7 @@
 document.getElementById("tab").addEventListener("click", openNav);
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-     document.body.style.backgroundColor = "rgba(230,230,230,0.90)";
+     document.body.style.backgroundColor = "white";
 }
 document.getElementById("closetab").addEventListener("click", closeNav);
 function closeNav() {
