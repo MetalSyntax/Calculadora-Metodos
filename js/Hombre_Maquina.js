ae35f164dc65710ac0071950bd5366ac5b9684d5
MetalSyntax <wonderdxk@gmail.com>
2017-04-10 18:42:56 -0400

    1.0.5

--- js/Hombre_Maquina.js
+++ js/Hombre_Maquina.js
@@ -39,3 +39,63 @@ function Numero_Tecnico(){
     $("#pc1").val(Math.floor(pc1));
     $("#pc2").val(Math.ceil(pc2));
 }
+/*******************************************************/
+/*Tiempo Necesario*/
+/*******************************************************/
+function Tiempo_Necesario(){
+    np = 0
+    tc = 0
+    pt = 0
+    e = 0
+    tn = 0
+    $("#np").each(function(index,value){
+		np = np + eval($(this).val());
+	});
+	$("#tc").each(function(index,value){
+		tc = tc + eval($(this).val());
+	});
+    $("#pt").each(function(index,value){
+		pt = pt + eval($(this).val());
+	});
+	$("#e").each(function(index,value){
+		e = e + eval($(this).val());
+	});
+    tn = np * (tc/pt)*(1/e)*(1/60);
+    $("#tn").val(tn);
+}
+/*******************************************************/
+/*Analisis de Costos*/
+/*******************************************************/
+function Analisis_de_Costos(){
+    
+    
+}
+/*******************************************************/
+/*Costos Totales*/
+/*******************************************************/
+function Costos_Totales() {
+	costo_total = 0
+	$(".importe_linea").each(
+		function(index, value) {
+			costo_total = costo_total + eval($(this).val());
+		});
+	$("#costo_total").val(costo_total);
+}
+function Nueva_Linea() {
+	$("#lineas").append('<input type="text" class="importe_linea" value="0"/><br/>');
+    input.placeholder = "Valor $";
+}
+/*******************************************************/
+/*Costo Unitario*/
+/*******************************************************/
+function Costo_Unitario(){
+	total_unidades = 0
+	Costo_Total = 0
+	$("#Unidades1").each(function(index,value){
+		total_unidades = total_unidades + eval($(this).val());
+	});
+	$("#Costo_Total1").each(function(index,value){
+		Costo_Total = Costo_Total + eval($(this).val());
+	});
+	$("#Costo_Unitario").val(Math.round (Costo_Total / total_unidades));
+}
