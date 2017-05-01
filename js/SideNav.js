bbdf5e500ae8b46720dcac9f2b670a9f4ad43847
MetalSyntax <wonderdxk@gmail.com>
2017-05-01 01:49:19 -0400

    1.0.7

--- js/SideNav.js
+++ js/SideNav.js
@@ -4,11 +4,14 @@
 document.getElementById("tab").addEventListener("click", openNav);
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-     document.body.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("overlay").style.display = "block";
 }
-document.getElementById("closetab").addEventListener("click", closeNav);
+
+/*document.getElementById("closetab").addEventListener("click", closeNav);*/
+document.getElementById("overlay").addEventListener("click", closeNav);
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "white";
-   
+    document.getElementById("overlay").style.display = "none";
 }
